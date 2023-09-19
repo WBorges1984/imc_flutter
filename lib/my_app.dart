93aa56bb176edba1c1c5b860cef23853e0313b8d
Willian.Borges <willianodhara@gmail.com>
2023-09-19 00:13:06 -0300

    Initial commit

---
+++ lib/my_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:imc_flutter/pages/login_pages.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        textTheme: GoogleFonts.robotoTextTheme(),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+    );
+  }
+}
