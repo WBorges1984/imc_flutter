5fa7cd7d125820579640bf3bca2e97ab700cc03f
Willian.Borges <willianodhara@gmail.com>
2023-09-25 23:25:11 -0300

    Estudando a melhor opção de guardar os dados

--- lib/pages/imc_anteriores.dart
+++ lib/pages/imc_anteriores.dart
@@ -13,7 +13,13 @@ class ImcAnteriores extends StatefulWidget {
 class _ImcAnterioresState extends State<ImcAnteriores> {
   var IcmCalculadas = ImcAnterioresRepository();
   var _imc = const <ResultadoImcAnteriores>[];
-  var counter = ["1", "2", "3"];
+  var resultadosAnteriores = [
+    {"imc": 25, "img": "MagrezaLeve"},
+    {"imc": 18.5, "img": "MagrezaModerada"},
+    {"imc": 25, "img": "MagrezaGrave"},
+  ];
+  var imc = 25;
+  var resultImage = "Saudavel";
 
   void ObterImc() async {
     _imc = await IcmCalculadas.listar();
@@ -24,10 +30,7 @@ class _ImcAnterioresState extends State<ImcAnteriores> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    carregaDados();
   }
-
-  carregaDados() {}
 
   @override
   Widget build(BuildContext context) {
@@ -44,15 +47,39 @@ class _ImcAnterioresState extends State<ImcAnteriores> {
           child: Column(
             children: [
               SizedBox(height: 10),
-              Text("Seu IMC"),
+              Text("Seu IMC: $imc"),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [Image.asset(AppImage.MagrezaModerada, height: 50)],
+                children: [
+                  resultImage == "MagrezaGrave"
+                      ? Image.asset(AppImage.MagrezaGrave, height: 50)
+                      : resultImage == "MagrezaModerada"
+                          ? Image.asset(AppImage.MagrezaModerada, height: 50)
+                          : resultImage == "MagrezaLeve"
+                              ? Image.asset(AppImage.MagrezaLeve, height: 50)
+                              : resultImage == "Saudavel"
+                                  ? Image.asset(AppImage.Saudavel, height: 50)
+                                  : resultImage == "Sobrepeso"
+                                      ? Image.asset(AppImage.SobrePeso,
+                                          height: 50)
+                                      : resultImage == "ObesidadeGrau1"
+                                          ? Image.asset(AppImage.ObesidadeGrau1,
+                                              height: 50)
+                                          : resultImage == "ObesidadeGrau2"
+                                              ? Image.asset(
+                                                  AppImage.ObesidadeGrau2,
+                                                  height: 50)
+                                              : resultImage == "ObesidadeGrau3"
+                                                  ? Image.asset(
+                                                      AppImage.ObesidadeGrau3,
+                                                      height: 50)
+                                                  : const Text(''),
+                ],
               ),
             ],
           ),
         ),
-      )
+      ),
     ]);
   }
 }
--- lib/shared/widgets/contagem_class.dart
+++ lib/shared/widgets/contagem_class.dart
@@ -1,11 +1,1 @@
-import 'package:flutter/material.dart';
-
-class ResultadoTela extends StatelessWidget {
-  var counter = [];
-  ResultadoTela({required this.counter});
-
-  @override
-  Widget build(BuildContext context) {
-    return Text("oi");
-  }
-}
+class ResultadoFeitos {}
